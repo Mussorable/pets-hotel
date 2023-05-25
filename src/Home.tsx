2067ce92ca33197480be42a731a574f3e3d96934
Oleh Diakov <redduck5601@gmail.com>
2023-05-25 22:12:42 +0200

    create forms, inputs, buttons, styles Sass

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,25 +1,62 @@
-import { useState } from "react";
 import Notification from "./components/Notification";
 import Form from "./components/Form";
 import Input from "./components/Input";
+import { Link } from "react-router-dom";
 
-export default function Home() {
-  const [isUpdated, setIsUpdated] = useState("");
+interface HomeProps {
+  isUpdated: boolean;
+}
 
+const Home: React.FC<HomeProps> = ({ isUpdated }) => {
   return (
-    <>
-      <h1>Pets</h1>
+    <div className="main-page">
+      <div className="heading-wrapper">
+        <h1 className="heading">Pets</h1>
+        <Link to="/pet-owners" className="input button">
+          Owners
+        </Link>
+      </div>
       {isUpdated && <Notification />}
-      <div className="add-pet-wrapper">
+      <div className="form-wrapper">
         <Form id="add-pet-form">
           <Input
             placeholder={"Pet name"}
             id={"pet-name"}
             type={"text"}
             srOnly={true}
+            label={true}
+          />
+          <Input
+            placeholder={"Pet breed"}
+            id={"pet-breed"}
+            type={"text"}
+            srOnly={true}
+            label={true}
+          />
+          <Input
+            placeholder={"Pet color"}
+            id={"pet-color"}
+            type={"text"}
+            srOnly={true}
+            label={true}
+          />
+          <Input
+            placeholder={"Pet owner"}
+            id={"pet-owner"}
+            type={"text"}
+            srOnly={true}
+            label={true}
+          />
+          <Input
+            label={false}
+            id={"submit-form"}
+            type={"submit"}
+            value="Add Pet"
           />
         </Form>
       </div>
-    </>
+    </div>
   );
-}
+};
+
+export default Home;
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,23 +1,45 @@
 import clsx from "clsx";
 
 interface InputProps {
-  placeholder: string;
+  label: boolean;
+  placeholder?: string;
   id: string;
   type: string;
-  srOnly: boolean;
+  srOnly?: boolean;
+  value?: string;
 }
 
-const Input: React.FC<InputProps> = ({ placeholder, id, type, srOnly }) => {
+const Input: React.FC<InputProps> = ({
+  label,
+  placeholder,
+  id,
+  type,
+  srOnly,
+  value,
+}) => {
   const labelClasses = clsx({
     "sr-only": srOnly,
   });
 
+  const inputClasses = clsx({
+    input: true,
+    button: !label,
+  });
+
   return (
     <>
-      <label htmlFor={id} className={labelClasses}>
-        {placeholder}
-      </label>
-      <input type={type} id={id} placeholder={placeholder} />
+      {label && (
+        <label htmlFor={id} className={labelClasses}>
+          {placeholder}
+        </label>
+      )}
+      <input
+        type={type}
+        id={id}
+        value={value}
+        className={inputClasses}
+        placeholder={placeholder}
+      />
     </>
   );
 };
