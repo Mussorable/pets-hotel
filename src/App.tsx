2b48a40c130e063b231d8b10101424fa8ba35cf3
Oleh Diakov <redduck5601@gmail.com>
2023-05-28 10:21:23 +0200

    add axios api connection, save pet form submit api

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import axios, { AxiosInstance } from "axios";
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
@@ -5,12 +6,20 @@ import Owners from "./Owners";
 
 function App() {
   const [isUpdated, setIsUpdated] = useState(false);
+  const api: AxiosInstance = axios.create({
+    baseURL:
+      "https://pet-hotel-375a8-default-rtdb.europe-west1.firebasedatabase.app/",
+    timeout: 1000,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home isUpdated={isUpdated} />} />
+          <Route path="/" element={<Home api={api} isUpdated={isUpdated} />} />
           <Route
             path="/pet-owners"
             element={<Owners isUpdated={isUpdated} />}
